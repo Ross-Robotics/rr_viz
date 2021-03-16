b941a6582116c1150025200eb288b7b11b5edcbb
Rebecca <rebecca@ross-robotics.co.uk>
2021-03-16 11:57:00 +0000

    attempting to get QwaypointListWidget integrated

--- rr_viz/src/ross/mission_editor/mission_editor.py
+++ rr_viz/src/ross/mission_editor/mission_editor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import sys
-from PyQt5.QtWidgets import QVBoxLayout, QWidget, QLabel, QGridLayout, QPushButton
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QLabel, QGridLayout, QPushButton, QListWidget, QFrame
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
+
+from ross.mission_editor.pathing.QWaypointListWidget import QWaypointListWidget
 
 class MissionEditor(QWidget):
     def __init__(self, parent):
@@ -16,9 +18,16 @@ class MissionEditor(QWidget):
         self.title_label.setAlignment(Qt.AlignRight)
         self.v_layout.addWidget(self.title_label)
 
+        # Title set up
+        self.editor_title_label = QLabel('Editor:')
+        self.editor_title_label.setFont(QFont('Ubuntu', 10, QFont.Bold))
+        self.v_layout.addWidget(self.editor_title_label)
+
+
         # Waypoint list
-        self.waypoint_list = QWidget()
-        self.v_layout.addWidget(self.waypoint_list)
+        # self.waypoint_list = QWidget()
+        self.waypoint_list = QWaypointListWidget()
+        self.v_layout.addWidget(self.waypoint_list, 5)
 
         # Buttons
         self.add_here_button = QPushButton('Add Here')
@@ -36,5 +45,26 @@ class MissionEditor(QWidget):
         self.button_layout.addWidget(self.save_mission_button, 2, 0)
         self.button_layout.addWidget(self.load_mission_button, 2, 1)
         self.v_layout.addLayout(self.button_layout)
+
+        # Line
+        self.line = QFrame()
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+        self.v_layout.addWidget(self.line)
+
+        # Send command
+        self.command_title = QLabel('Send Command:')
+        self.command_title.setFont(QFont('Ubuntu', 10, QFont.Bold))
+        self.v_layout.addWidget(self.command_title)
+
+        # Command buttons
+        self.h_layout = QHBoxLayout()
+        self.go_to_selected_button = QPushButton('Go to Selected')
+        self.go_to_all_button = QPushButton('Go to All')
+        self.send_mission_button = QPushButton('Send Mission')
+        self.h_layout.addWidget(self.go_to_selected_button)
+        self.h_layout.addWidget(self.go_to_all_button)
+        self.h_layout.addWidget(self.send_mission_button)
+        self.v_layout.addLayout(self.h_layout)
 
         self.setLayout(self.v_layout)
