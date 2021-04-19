d7101bb02f5efc7367b605929d6ae8d2e3f81d35
Rebecca <rebecca@ross-robotics.co.uk>
2021-04-19 16:22:14 +0100

    fix filepath take 2

--- rr_viz/src/ross/mission_dashboard/path_navigation_tools.py
+++ rr_viz/src/ross/mission_dashboard/path_navigation_tools.py
@@ -130,9 +130,9 @@ class PathNavigationTools(QWidget):
             # path_files = file_management.get_files(file_path_to_search, ".txt")
             path_to_follow, ok = QInputDialog.getItem(self, "Select path to follow", "Available paths:", path_files, 0, False)
             path = file_path_to_search + "/" + path_to_follow
-            print(path_to_follow)
+            print(path)
             if ok:
-                self.tracking_action.send_goal(TrackPathGoal(file_path=path_to_follow))
+                self.tracking_action.send_goal(TrackPathGoal(file_path=path))
         else:
             rospy.logwarn_throttle(10, "Failed to get path files")
 
