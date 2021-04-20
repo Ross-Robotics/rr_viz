1cc32ed8e9357b5655ceb614079d1ed022005af2
Pepe <pepe@robosynthesis.com>
2021-04-20 10:55:55 +0100

    Addinch loader check

--- rr_viz/src/webview/rr_webview.py
+++ rr_viz/src/webview/rr_webview.py
@@ -5,16 +5,30 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebKitWidgets import QWebView
+from PyQt5.QtWebKitWidgets import QWebPage
 import rospy
+from enum import Enum
 
 
 class RRQWebView(QWebView):
+
     def __init__(self, parent=None):
         super(self.__class__, self).__init__(parent)
-        self.setWindowTitle('Loading Web UI...')
+        self.loading_stages = Enum('Loading_stages', 'CONNECTING AUTENTICATING FORMATTING FINISHED')
+        self.loading_stage = self.loading_stages.CONNECTING
+        self.timer_period = 500
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self._after_loading_timer)
+        self.timer.setSingleShot(True)
         self.titleChanged.connect(self.adjustTitle)
         self.load(rospy.get_param(
             "~web_ui_url", "https://www.robosynthesis.com/"))
+        self.loadFinished.connect(self._on_load_finished)
+        print("Remote Screen ",self.loading_stage)
+
+    class WebPage(QWebPage):
+        def javaScriptConsoleMessage(self, msg, line, source):
+            print '%s line %d: %s' % (source, line, msg)
 
     def load(self, url):
         self.setUrl(QUrl(url))
@@ -22,5 +36,53 @@ class RRQWebView(QWebView):
     def adjustTitle(self):
         self.setWindowTitle(self.title())
 
+    def _on_load_finished(self):
+        self.loading_stage = self.loading_stages.AUTENTICATING
+        print("Remote Screen ",self.loading_stage)
+        self.timer.start(self.timer_period)
 
-# class OverlayedWidget(QStackedWidget):
+    def _after_loading_timer(self):
+        print("Remote Screen ",self.loading_stage)
+        if (self._is_login_page()):
+            self._login()
+            self.timer.start(self.timer_period)
+        else:
+            self._remove_gui_elements()
+
+    def _is_login_page(self):
+        frame = self.page().currentFrame()
+        return bool(frame.evaluateJavaScript("document.getElementsByClassName('login').length"))
+
+    def _login(self):
+        frame = self.page().currentFrame()
+        frame.evaluateJavaScript("document.getElementById('mat-input-0').value = 'advanced'")
+        frame.evaluateJavaScript("document.getElementById('mat-input-1').value = 'symetricas'")
+        frame.evaluateJavaScript("document.getElementById('login-button').click()")
+        self.loading_stage = self.loading_stages.FORMATTING
+
+    def _remove_gui_elements(self):
+        frame = self.page().currentFrame()
+        if self._is_remote_screen_loaded():
+            frame.evaluateJavaScript("document.getElementsByClassName('nav')[0].parentNode.removeChild(document.getElementsByClassName('nav')[0])")
+            frame.evaluateJavaScript("document.getElementsByClassName('status-wrapper StatusBarGeneral')[0].parentNode.removeChild(document.getElementsByClassName('status-wrapper StatusBarGeneral')[0])")
+            frame.evaluateJavaScript("document.getElementsByClassName('doseinfo')[0].parentNode.removeChild(document.getElementsByClassName('doseinfo')[0])")
+            frame.evaluateJavaScript("document.getElementsByTagName('h1')[0].style.display = 'none'")
+            frame.evaluateJavaScript("document.getElementsByTagName('p')[0].style.display = 'none'")
+            self.loading_stage = self.loading_stages.FINISHED
+            return True
+        else:
+            self.timer.start(self.timer_period)
+            return False
+
+    def _is_remote_screen_loaded(self):
+        frame = self.page().currentFrame()
+        if frame.evaluateJavaScript("document.getElementsByClassName('nav').length"):
+            if frame.evaluateJavaScript("document.getElementsByClassName('status-wrapper StatusBarGeneral').length"):
+                if frame.evaluateJavaScript("document.getElementsByClassName('doseinfo').length"):
+                    if frame.evaluateJavaScript("document.getElementsByTagName('h1').length"):
+                        if frame.evaluateJavaScript("document.getElementsByTagName('p').length"):
+                            return True
+        return False
+
+
+#document.getElementsByTagName('body')[0].appendChild(img)
--- rr_viz/src/webview/rr_webview_tab.py
+++ rr_viz/src/webview/rr_webview_tab.py
@@ -7,26 +7,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebKitWidgets import QWebView
 import rospy
-#check https://stackoverflow.com/questions/11060439/filling-out-a-form-using-pyqt-and-qwebview
-#document.getElementById("search-input").value='test2'
-#removing the top bar:
-#document.getElementsByClassName('nav')[0].parentNode.removeChild(document.getElementsByClassName('nav')[0])
-#removes the grey line with date/time
-#document.getElementsByClassName('status-wrapper StatusBarGeneral')[0].parentNode.removeChild(document.getElementsByClassName('status-wrapper StatusBarGeneral')[0])
-#clicks the login button
-#document.getElementById('login-button').click()
-# fills the password
-#document.getElementById('mat-input-1').value = 'symetricas'
-#fuills username
-# document.getElementById('mat-input-0').value = 'advanced'
-#http://10.42.0.59/ui/en/hosts/sn23n-180004/remote-control
 
-#work plan:
-# load the intended URL
-# check if we got to the correct URL or if we are in the login screen
-# if we are in the login screen input username and password
-#if we are in the indeded screen lets remove unwanted elements
-# profit
 
 class RRQWebViewTab(QWebView):
     def __init__(self, parent=None):
@@ -41,7 +22,6 @@ class RRQWebViewTab(QWebView):
         self.loadFinished.connect(self._on_load_finished)
 
 
-
     def load(self, url):
         self.setUrl(QUrl(url))
 
@@ -49,18 +29,28 @@ class RRQWebViewTab(QWebView):
     def adjustTitle(self):
         self.setWindowTitle(self.title())
 
+
     def _on_load_finished(self):
         print("FINISHED LOADING")
-        print(self.url().toString())
         self.timer.start(3000)
 
 
     def _after_loading_timer(self):
-        print(self.url().toString())
+        print("Logging into the Verifinder ... ")
+        if (self._is_login_page()):
+            self._login()
+
+
+    def _is_login_page(self):
+        frame = self.page().currentFrame()
+        return bool(frame.evaluateJavaScript("document.getElementsByClassName('login').length"))
+
+    def _login(self):
         frame = self.page().currentFrame()
         frame.evaluateJavaScript("document.getElementById('mat-input-0').value = 'advanced'")
         frame.evaluateJavaScript("document.getElementById('mat-input-1').value = 'symetricas'")
         frame.evaluateJavaScript("document.getElementById('login-button').click()")
 
-
-# class OverlayedWidget(QStackedWidget):
+    def _remove_gui_elements(self):
+        frame = self.page().currentFrame()
+        pass
