6d2c57cd1f7c6c6d8cf821efed2f86aee930ad4b
Pepe <pepe@robosynthesis.com>
2021-04-26 11:56:10 +0100

    CEF initial implementation

--- rr_viz/src/webview/rr_webview_tab.py
+++ rr_viz/src/webview/rr_webview_tab.py
@@ -1,4 +1,8 @@
 import os
+import sys
+import ctypes
+import platform
+from cefpython3 import cefpython as cef
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import QUrl
 from PyQt5.QtCore import *
@@ -8,6 +12,8 @@ from PyQt5.QtWebKitWidgets import QWebView
 from PyQt5.QtWebKitWidgets import QWebPage
 import rospy
 import rospkg
+
+
 
 #http://devtools.fg.oisin.rc-harwell.ac.uk/nightly/7.0/ccp4-src-7.0/checkout/PyQt-x11-gpl-4.11.2/doc/html/qwebview.html
 
@@ -24,6 +30,13 @@ class RRQWebViewTab(QWidget):
         self.status_label.setHidden(True)
         self.status_label.setAlignment(Qt.AlignCenter)
 
+        #CEF stuff
+        sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
+        # app = self.CefApplication(sys.argv)
+        self.cef_widget = self.CefWidget()
+
+        self.web_gui = self.RRQWebTab()
+
         self.logo_label = QLabel(self)
         self.logo_label.setPixmap(pixmap)
         self.logo_label.setAlignment(Qt.AlignCenter)
@@ -32,19 +45,29 @@ class RRQWebViewTab(QWidget):
         self.loading_layout.addWidget(self.logo_label,1)
         self.loading_layout.addWidget(self.status_label,2)
         main_layout.addLayout(self.loading_layout)
-        self.web_gui = self.RRQWebTab()
-        main_layout.addWidget(self.web_gui,1)
-        self.timer_period = 500
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self._visibility_manager)
+        #self.web_gui = self.RRQWebTab()
+        #main_layout.addWidget(self.web_gui,1)
+        #self.container = QWidget.createWindowContainer(self.cef_widget.hidden_window, parent=self)
+        #main_layout.addWidget(self.cef_widget,2)
+
+        self.tab_timer_period = 500
+        self.tab_timer = QTimer(self)
+        self.tab_timer.timeout.connect(self._visibility_manager)
         self.setLayout(main_layout)
-        self.timer.start(self.timer_period)
+        self.cef_widget.embedBrowser()
+        self.cef_container = QWidget.createWindowContainer(self.cef_widget.hidden_window, parent=self)
+        main_layout.addWidget(self.cef_container,2)
+        self.tab_timer.start(self.tab_timer_period)
+
 
     def _visibility_manager(self):
-        self.status_label.setHidden(not self.web_gui.is_hidden)
-        self.logo_label.setHidden(not self.web_gui.is_hidden)
-        self.loading_layout.setEnabled(self.web_gui.is_hidden)
-        self.status_label.setText(self.web_gui.gui_state + " ...")
+        # self.status_label.setHidden(not self.web_gui.is_hidden)
+        # self.logo_label.setHidden(not self.web_gui.is_hidden)
+        # self.loading_layout.setEnabled(self.web_gui.is_hidden)
+        # self.status_label.setText(self.web_gui.gui_state + " ...")
+        self.cef_widget.resizeWindow(self.cef_container.width() ,self.cef_container.height())
+
+
 
     class RRQWebTab(QWebView):
         def __init__(self, parent=None):
@@ -90,7 +113,7 @@ class RRQWebViewTab(QWidget):
             elif self.current_url.find(self.gui_url_dict.get("home_page")) >= 0:
                 if self.is_finished_loading:
                     self.gui_state = "Remote screen fully loaded"
-                    self.is_hidden = False
+                    #self.is_hidden = False
                     self.gui_state = "Opening remote screen"
             if self.url_loading_state == "Failed":
                 self._load( self.current_url)
@@ -150,3 +173,33 @@ class RRQWebViewTab(QWidget):
             def javaScriptConsoleMessage(self, msg, line, source):
                 print("JS CONSOLE OUTPUT")
                 print '%s line %d: %s' % (source, line, msg)
+
+
+    class CefWidget(QWidget):
+        def __init__(self, parent=None):
+            # noinspection PyArgumentList
+            super(self.__class__, self).__init__(parent)
+            cef.Initialize()
+            self.parent = parent
+            self.hidden_window = None  # Required for PyQt5 on Linux
+            self.window_info = cef.WindowInfo()
+            self.cef_timer_period = 10
+            self.cef_timer = QTimer(self)
+            self.cef_timer.timeout.connect(self._cef_on_timer)
+            self.cef_timer.start(self.cef_timer_period)
+
+        def _cef_on_timer(self):
+            cef.MessageLoopWork()
+
+        def embedBrowser(self):
+            self.hidden_window = QWindow()
+            self.window_info.SetAsChild(int(self.hidden_window.winId()))
+            self.browser = cef.CreateBrowserSync(self.window_info, url="http://10.42.0.59")
+
+        def resizeWindow(self,width,height):
+            self.browser.SetBounds(0,0,width,height)
+
+        def resizeEvent(self, event):
+            size = event.size()
+            self.browser.SetBounds(self.x, self.y, size.width(), size.height())
+            self.browser.NotifyMoveOrResizeStarted()
