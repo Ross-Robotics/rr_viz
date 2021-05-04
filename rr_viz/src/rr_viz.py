d82f20a9f930eaccc891a924c178bdfe99455ac7
Rebecca <rebecca@ross-robotics.co.uk>
2021-05-04 12:52:49 +0100

    Add in connection status

--- rr_viz/src/rr_viz.py
+++ rr_viz/src/rr_viz.py
@@ -13,8 +13,6 @@ from helpers import rr_qt_helper
 from fping import FastPing
 
 class RossRoboticsRViz(QMainWindow):
-    # ross_loss_signal = QtCore.pyqtSignal(bool)
-
     def __init__(self, parent=None):
         super(QMainWindow, self).__init__(parent)
         self.setWindowTitle("Ross Robotics RViz")
@@ -26,69 +24,11 @@ class RossRoboticsRViz(QMainWindow):
         self.tabWidget = RRVizTabs(self)
         self.setCentralWidget(self.tabWidget)
 
-        # self.ross_loss_signal.connect(self.ros_loss_slot)
-        self.ros_loss_triggered = False
-        self.fp = FastPing()
-        self.rosMasterIP = '10.42.0.1'
-        self.fp_status = "alive"          
-        
-        self.timer_period = 1000
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.ros_is_up)
-        self.timer.start(self.timer_period)
-
-    def ros_is_up(self):
-        result = self.fp.ping(targets=[self.rosMasterIP])
-        ros_master_hostname = ''.join(result.keys())
-        if not self.fp_status in result[ros_master_hostname] and not self.ros_loss_triggered:
-            rospy.logerr("Connection to ROS MASTER has been lost")
-            self.ros_loss_triggered = True
-            # msg = QMessageBox()
-            # msg.setIcon(QMessageBox.Warning)
-            # msg.setText(
-            #     "RR_VIZ lost connection to the ROS MASTER. Please wait")
-            # msg.setWindowTitle("Connection Lost")
-            # exit_rr_viz = msg.addButton('Exit RR_VIZ', QMessageBox.AcceptRole)
-            # msg.exec_()
-            # if msg.clickedButton() == exit_rr_viz:
-            #     sys.exit()            
-        elif self.fp_status in result[ros_master_hostname] and self.ros_loss_triggered:
-            rospy.loginfo("Connection to ROS MASTER has been restored")
-            self.ros_loss_triggered = False
-            
-
-    # def ros_loss_slot(self, flag):
-    #     msg = QMessageBox()
-    #     print("flag: {}".format(flag))
-    #     # print("trig: {}".format(self.ros_loss_triggered))
-    #     if not flag and not self.ros_loss_triggered:
-    #         # print("no ros")
-    #         self.ros_loss_triggered = True
-
-    #         msg.setIcon(QMessageBox.Warning)
-    #         msg.setText(
-    #             "RR_VIZ lost connection to the ROS MASTER. Please wait")
-    #         msg.setWindowTitle("Connection Lost")
-    #         exit_rr_viz = msg.addButton('Exit RR_VIZ', QMessageBox.AcceptRole)
-    #         continue_rr_viz = msg.addButton('Continue', QMessageBox.AcceptRole)
-    #         msg.exec_()
-    #         if msg.clickedButton() == exit_rr_viz:
-    #             sys.exit()
-    #     elif flag and self.ros_loss_triggered:
-    #         self.ros_loss_triggered = False
-    #         # print("BACK")
-    #         msg.setText("Connection to ROS MASTER is re-established. Click Continue to proceed")
-    #         msg.exec_()
-
-
 if __name__ == '__main__':
     rospy.init_node("rr_viz")
     app = QApplication(sys.argv)
     main_window = RossRoboticsRViz()
     main_window.show()
-    # ros_state_checker = rr_qt_helper.StateCheckerTimer(
-    #     main_window.ros_is_up, main_window.ross_loss_signal, Hz=1)
-    # ros_state_checker.start()
-    # Needed for ctrl+c to work on pyqt applications
+
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     sys.exit(app.exec_())
--- rr_viz/src/rr_viz_tabs.py
+++ rr_viz/src/rr_viz_tabs.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from PyQt5.QtWidgets import QTabWidget, QVBoxLayout, QWidget, QHBoxLayout, QLabel
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtCore import Qt, QTimer
 
 from rviz_tabs import RvizTabs
 from ross.rr_interactive_tools import RRInteractiveTools
 import managers.file_management as file_management
+import subprocess
 
 class RRVizTabs(QWidget):
     def __init__(self, parent):
@@ -22,9 +23,26 @@ class RRVizTabs(QWidget):
         self.rr_interactive_tools_tab.v_layout2 = QVBoxLayout(self)
 
         self.rr_interactive_tools_tab.v_layout1.addWidget(RvizTabs(self))
+
+        # Set up Connection status labels
+        self.connection_h_layout = QHBoxLayout()
+        self.connection_label_text = QLabel('Connection status:')
+        self.connection_label_text.setFont(QFont('Ubuntu', 14, QFont.Bold))
+        self.connection_label_text.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        
+        self.connection_status = QLabel('Connection lost')
+        self.connection_status.setFont(QFont('Ubuntu', 14, QFont.Bold))
+        self.connection_status.setStyleSheet("color: red")
+        self.connection_status.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+
+        self.connection_h_layout.addWidget(self.connection_label_text, 7)
+        self.connection_h_layout.addWidget(self.connection_status, 3)
+        self.rr_interactive_tools_tab.v_layout2.addLayout(self.connection_h_layout,1)
      
+        # Add rr interactive tools to layout
         self.rr_interactive_tools_tab.v_layout2.addWidget(RRInteractiveTools(self),7)
 
+        # Set up logo
         self.logo_label = QLabel(self)
         logo_path = file_management.get_rrviz_resdir() + "/logo.png"
         logo_pixmap = QPixmap(logo_path)
@@ -33,8 +51,9 @@ class RRVizTabs(QWidget):
         
         self.logo_label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
 
-        self.rr_interactive_tools_tab.v_layout2.addWidget(self.logo_label, 3)
+        self.rr_interactive_tools_tab.v_layout2.addWidget(self.logo_label, 2)
 
+        # Add vertical layouts to the horizontal layout
         self.rr_interactive_tools_tab.h_layout.addLayout(self.rr_interactive_tools_tab.v_layout1, 7)
         self.rr_interactive_tools_tab.h_layout.addLayout(self.rr_interactive_tools_tab.v_layout2, 3)
 
@@ -42,3 +61,28 @@ class RRVizTabs(QWidget):
 
         self.main_window_layout.addWidget(self.main_window_tabs)
         self.setLayout(self.main_window_layout)
+
+        # Variables required to detect if connected to ROS MASTER
+        self.ros_loss_triggered = True
+        self.rosMasterIP = '192.168.10.100'
+        self.subprocess_command = 'fping -nV -t 50 ' + self.rosMasterIP
+        self.fp_status = "alive"  
+
+        # Set up timer 
+        self.timer_period = 1500
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.ros_is_up)
+        self.timer.start(self.timer_period)
+
+    def ros_is_up(self):
+        process = subprocess.Popen(self.subprocess_command, shell=True, stdout=subprocess.PIPE)
+        stdout = process.communicate()[0]
+
+        if not self.fp_status in stdout and not self.ros_loss_triggered:
+            self.connection_status.setText("Connection lost")
+            self.connection_status.setStyleSheet("color: red")
+            self.ros_loss_triggered = True
+        elif self.fp_status in stdout and self.ros_loss_triggered:
+            self.connection_status.setText("Connected")
+            self.connection_status.setStyleSheet("color: green")
+            self.ros_loss_triggered = False
