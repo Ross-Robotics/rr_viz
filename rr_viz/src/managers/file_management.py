379358b94e42b314f21aecfed8b883d14cd32365
Rebecca <rebecca@ross-robotics.co.uk>
2021-05-05 12:21:05 +0100

    update checking and creating directories

--- rr_viz/src/managers/file_management.py
+++ rr_viz/src/managers/file_management.py
@@ -5,6 +5,8 @@ import rospkg
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QMessageBox
 
 RR_VIZ_USER_DIR = os.path.expanduser("~/Desktop/RossRobotics")
+RR_VIZ_MISSION_DIR = RR_VIZ_USER_DIR + "/mission"
+RR_VIZ_MAP_IMAGES_DIR = RR_VIZ_USER_DIR + "/map_images"
 
 def get_rrviz_dir():
     rospack = rospkg.RosPack()
@@ -18,17 +20,23 @@ def get_rrviz_cfgdir():
 
 def get_mission_files_dir():
     check_user_dir()
-    return RR_VIZ_USER_DIR + "/mission"
+    return RR_VIZ_MISSION_DIR
 
 def get_map_images_dir():
     check_user_dir()
-    return RR_VIZ_USER_DIR + "/map_images"
+    return RR_VIZ_MAP_IMAGES_DIR
 
 def check_user_dir():
     # Check if  the ~/.rr directory is setup and if it isnt, set it up.
     if not os.path.exists(RR_VIZ_USER_DIR):
-        create_user_dir()
+        create_ross_robotics_dir()
         rospy.logerr(RR_VIZ_USER_DIR + " directory did not exist, and has been created. It is needed for RR_VIZ to work.")
+    if not os.path.exists(RR_VIZ_MISSION_DIR):
+        create_mission_dir()
+        rospy.logerr(RR_VIZ_MISSION_DIR + " directory did not exist, and has been created. It is needed for RR_VIZ to work.")
+    if not os.path.exists(RR_VIZ_MAP_IMAGES_DIR):
+        create_map_images_dir()
+        rospy.logerr(RR_VIZ_MAP_IMAGES_DIR + " directory did not exist, and has been created. It is needed for RR_VIZ to work.")
 
 def get_files(dir_path, extension):
     if not os.path.exists(dir_path):
@@ -40,7 +48,11 @@ def get_files(dir_path, extension):
         files.append(f)
     return files
 
-def create_user_dir():
+def create_ross_robotics_dir():
     os.mkdir(RR_VIZ_USER_DIR)
-    os.mkdir(RR_VIZ_USER_DIR +"/mission")
-    os.mkdir(RR_VIZ_USER_DIR + "/map_images") 
+
+def create_mission_dir():
+    os.mkdir(RR_VIZ_MISSION_DIR)
+
+def create_map_images_dir():
+    os.mkdir(RR_VIZ_MAP_IMAGES_DIR) 
