997d1fb2f74e3953f4aa6140f19e5378855497d8
Pepe <pepe@robosynthesis.com>
2021-06-02 13:37:23 +0100

    Table populating working just missing a test with live stream

--- rr_viz/src/ross/explosive_ace_id/explosive_ace_id.py
+++ rr_viz/src/ross/explosive_ace_id/explosive_ace_id.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QLabel, QPushButton, QListWidget, QLineEdit, QMessageBox, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QLabel, QPushButton, QListWidget, QLineEdit, QMessageBox, QTableWidget, QTableWidgetItem, QHeaderView
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import *
@@ -8,15 +8,23 @@ from PyQt5 import QtCore
 import sys
 import telnetlib
 import rospy
+import re
 
 class ExplosiveAceID(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
 
         self.ace_id_hostname = rospy.get_param("~ace_id_hostname", "92.207.233.130")
-        self.ace_id_port = rospy.get_param("~ace_id_port", "92.207.233.130")
+        self.ace_id_port = rospy.get_param("~ace_id_port", "8007")
         self.tn = telnetlib.Telnet()
+        self.telnet_read = ""
+        self.results_number = 0
+        self.no_results_dict = {
+            "No materials found":0.00
+        }
+        self.results_dict = {}
         self.connected = False
+        self.started_acquisition = False
 
         self.v_layout = QVBoxLayout()
 
@@ -25,10 +33,16 @@ class ExplosiveAceID(QWidget):
         self.title_label.setFont(QFont('Ubuntu', 11, QFont.Bold))
         self.title_label.setAlignment(Qt.AlignRight)
         self.v_layout.addWidget(self.title_label)
+        # Status
+        self.status_label = QLabel('Not connected')
+        self.status_label.setFont(QFont('Ubuntu', 10, QFont.Bold))
+        self.status_label.setAlignment(Qt.AlignRight)
+        self.v_layout.addWidget(self.status_label)
+
         # Table
         self.table = QTableWidget()
-
         self.v_layout.addWidget(self.table)
+        self._init_table()
 
         # Buttons
         self.h_layout_buttons = QHBoxLayout()
@@ -36,7 +50,7 @@ class ExplosiveAceID(QWidget):
         self.connect_button = QPushButton("Connect")
         self.connect_button.pressed.connect(self.connect)
 
-        self.acquire_button = QPushButton("Acquire")
+        self.acquire_button = QPushButton("Begin Acquisition")
         self.acquire_button.setEnabled(False)
         self.acquire_button.pressed.connect(self.acquire)
 
@@ -46,16 +60,32 @@ class ExplosiveAceID(QWidget):
         self.v_layout.addLayout(self.h_layout_buttons)
 
         self.setLayout(self.v_layout)
-        self.timer_period = 500
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self._visibility_manager)
-        self.timer.start(self.timer_period)
+        #Timers
+        self.visibility_timer_period = 500
+        self.visibility_timer = QTimer(self)
+        self.visibility_timer.timeout.connect(self._visibility_manager)
+        self.visibility_timer.start(self.visibility_timer_period)
+
+        self.acquisition_timer_period = 500
+        self.acquisition_timer = QTimer(self)
+        self.acquisition_timer.timeout.connect(self._acquisition_timer)
+
+        self.results_timer_period = 1000
+        self.results_timer = QTimer(self)
+        self.results_timer.timeout.connect(self._results_timer)
 
     def _visibility_manager(self):
         if self.connected == True:
-            self.acquire_button.setEnabled(True)
+            if self.started_acquisition == False:
+                self.acquire_button.setEnabled(True)
+            if self.started_acquisition == True:
+                self.connect_button.setEnabled(False)
+            else:
+                self.connect_button.setEnabled(True)
+            self.status_label.setText('Connected')
         else:
             self.acquire_button.setEnabled(False)
+            self.status_label.setText('Not connected')
         pass
 
     def connect(self):
@@ -66,27 +96,111 @@ class ExplosiveAceID(QWidget):
             print("Cannot connect to the ACE-ID host machine")
             self.connected = False
 
+    def _acquisition_timer(self):
+        print("ACE-ID Waiting for acquisition.")
+        self.telnet_read = self.telnet_read + self.tn.read_very_eager()
+        self.telnet_read = self.telnet_read.replace("\n",'')
+        substrings = self.telnet_read.split("\r")
+        for i in range(len(substrings)):
+            if substrings[i] == "DONE":
+                print("ACE-ID Acquisition FINISHED.")
+                self.telnet_read = ""
+                self.started_acquisition = False
+                self.acquisition_timer.stop()
+                self.get_results()
+
+
+    def get_results(self):
+        self.tn.write("sync results" + "\n")
+        self.results_timer.start(self.results_timer_period)
+        pass
+
+    def _results_timer(self):
+        print("ACE-ID Waiting for results.")
+        self.telnet_read = self.telnet_read + self.tn.read_very_eager()
+        self.telnet_read = self.telnet_read.replace("\n",'')
+        substrings = self.telnet_read.split("\r")
+        for i in range(len(substrings)):
+            if substrings[i].find("tsGetComponents returned") > -1:
+                self.results_number = int(substrings[i].split("returned ")[1])
+                print("ACE-ID Results received.")
+                print("Components found: ", self.results_number)
+                self.telnet_read = ""
+                self.results_timer.stop()
+                self._results_parser()
+                self.fill_table()
+        pass
+
+    def _results_parser(self):
+        example_string = """>sync results
+
+Getting search results...
+
+  tsGetComponents returned 2
+
+  [ 0] = Toluene [Quality=0.96] [Contribution=0.00]
+
+         UCID = 108883, CAS Text = 108-88-3
+
+         Entry 590 from Library 'ACE-ID_Std.lib'
+
+  [ 1] = Acetonitrile [Quality=0.24] [Contribution=0.00]
+
+         UCID = 75058, CAS Text = 75-05-8
+
+         Entry 106 from Library 'ACE-ID_Std.lib' """
+
+        for line in example_string.split("\n"):
+            if "Quality" in line:
+                result_line = line.strip()
+                result_line = result_line.split("] = ")[1].split("] [Contribution")[0]
+                material = result_line.split(" [Quality=")[0]
+                quality = result_line.split(" [Quality=")[1]
+                self.results_dict[material] = quality
+                self.results_number = self.results_number + 1
+                print(result_line)
+                print(material,quality)
+                print(self.results_dict)
+
+    def fill_table(self):
+        if self.results_number == 0:
+            self.results_dict = self.no_results_dict
+        for x in self.results_dict.keys():
+            row_count = self.table.rowCount()
+            self.table.insertRow(self.table.rowCount())
+            material_item = QTableWidgetItem(x)
+            material_item.setTextAlignment(Qt.AlignCenter)
+            material_item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled )
+            quality_item = QTableWidgetItem(str(self.results_dict[x]))
+            quality_item.setTextAlignment(Qt.AlignCenter)
+            quality_item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled )
+            self.table.setItem(row_count,0,material_item)
+            self.table.setItem(row_count,1,quality_item)
+        pass
+
+    def clear_table(self):
+        while self.table.rowCount() > 0:
+            self.table.removeRow(self.table.rowCount()-1)
+
+    def _init_table(self):
+        y = 0
+        while y < 2:
+            self.table.insertColumn(y)
+            y += 1
+
+        column_titles = ['Material','Quality']
+        self.table.setHorizontalHeaderLabels(column_titles)
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.Stretch)
+
     def acquire(self):
         if self.connected == True:
-            self.tn.write("sync results" + "\n")
-            print(tn.read_all())
-            data = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+            if self.started_acquisition == False:
+                self.tn.write("RC Start" + "\n")
+                self.started_acquisition = True
+                self.acquisition_timer.start(self.acquisition_timer_period)
+                self.acquire_button.setEnabled(False)
+                self.connect_button.setEnabled(False)
 
-            x = 0
-            while x < len(data):
-                self.table.insertRow(x)
-                x += 1
-
-            y = 0
-            while y < len(data[0]):
-                self.table.insertColumn(y)
-                y += 1
-
-            column_titles = ['x','y','z']
-            self.table.setHorizontalHeaderLabels(column_titles)
-
-            for row in data:
-                index = data.index(row)
-                self.table.setItem(index,0,QTableWidgetItem(str(row[0])))
-                self.table.setItem(index,1,QTableWidgetItem(str(row[1])))
-                self.table.setItem(index,2,QTableWidgetItem(str(row[2])))
+            self.clear_table()
