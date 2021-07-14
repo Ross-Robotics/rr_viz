50d618208705ce1ac38500891c0b7d1cf12a7972
Rebecca <rebecca@ross-robotics.co.uk>
2021-07-14 19:26:03 +0100

    update connection status to signal strength and latency

--- rr_viz/src/rr_viz_tabs.py
+++ rr_viz/src/rr_viz_tabs.py
@@ -12,8 +12,7 @@ from ross.esm.environmental_sensing_module import EnvironmentalSensingModule
 import managers.file_management as file_management
 import subprocess
 from sensor_msgs.msg import BatteryState
-from rr_custom_msgs.msg import Clock
-from std_msgs.msg import Time as RosTime
+from rr_custom_msgs.msg import CommsData, Ping
 import rospy
 
 class RRVizTabs(QWidget):
@@ -41,26 +40,37 @@ class RRVizTabs(QWidget):
 
         # Set up status labels
         self.status_h_layout = QHBoxLayout()
-        self.connection_label_text = QLabel('Connection status:')
-        self.connection_label_text.setFont(QFont('Ubuntu', 14, QFont.Bold))
-        self.connection_label_text.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.signal_strength_label = QLabel('Signal Strength:')
+        self.signal_strength_label.setFont(QFont('Ubuntu', 11, QFont.Bold))
+        self.signal_strength_label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
         
-        self.connection_status = QLabel('Connecting')
-        self.connection_status.setFont(QFont('Ubuntu', 14, QFont.Bold))
-        self.connection_status.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        self.signal_strength = QLabel('Connecting')
+        self.signal_strength.setFont(QFont('Ubuntu', 11, QFont.Bold))
+        self.signal_strength.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+
+        self.latency_label = QLabel('Latency:')
+        self.latency_label.setFont(QFont('Ubuntu', 11, QFont.Bold))
+        self.latency_label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        
+        self.latency = QLabel('0')
+        self.latency.setFont(QFont('Ubuntu', 11, QFont.Bold))
+        self.latency.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
 
         self.battery_label = QLabel('Battery Level:')
-        self.battery_label.setFont(QFont('Ubuntu', 14, QFont.Bold))
+        self.battery_label.setFont(QFont('Ubuntu', 11, QFont.Bold))
         self.battery_label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
 
         self.battery_level = QLabel('N/A')
-        self.battery_level.setFont(QFont('Ubuntu', 14, QFont.Bold))
+        self.battery_level.setFont(QFont('Ubuntu', 11, QFont.Bold))
         self.battery_level.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
 
-        self.status_h_layout.addWidget(self.connection_label_text, 3)
-        self.status_h_layout.addWidget(self.connection_status, 3)
+        self.status_h_layout.addWidget(self.signal_strength_label, 2)
+        self.status_h_layout.addWidget(self.signal_strength, 2)
 
-        self.status_h_layout.addWidget(self.battery_label, 3)
+        self.status_h_layout.addWidget(self.latency_label, 2)
+        self.status_h_layout.addWidget(self.latency, 2)
+
+        self.status_h_layout.addWidget(self.battery_label, 1)
         self.status_h_layout.addWidget(self.battery_level, 1)
 
         self.rr_interactive_tools_tab.v_layout2.addLayout(self.status_h_layout,1)
@@ -93,62 +103,22 @@ class RRVizTabs(QWidget):
         self.battery_sub = rospy.Subscriber(self.battery_topic, BatteryState, self.battery_cb)
         self.low_battery_popup.connect(self.bat_message_popup)
 
-        # Set up Connection variables
-        self.clock_topic = rospy.get_param("/core_clock_sub","/mk3_core/clock")
-        self.clock_freq = rospy.get_param("/mk3_core/core_clock_publisher/publish_frequency", 10)
-        self.poor_connection_threshold = rospy.get_param("/core_clock_poor_connection", 10)
-        self.lost_connection_threshold = rospy.get_param("/core_clock_lost_connection", 20)
-        self.last_received_time = RosTime()
-        self.first_value = False
-        self.get_first_msg = False
-        self.latency = 0
-        self.read_time = RosTime()
-        self.no_time_change = 0
-        freq = float(self.clock_freq) * 2
-        self.timer_period = 1.0 / freq
+        # Set up signal callback
+        self.comms_topic = rospy.get_param("/comms_topic","/rr_ocu/comms_data")
+        self.comms_sub = rospy.Subscriber(self.comms_topic, CommsData, self.comms_cb)
 
-        # Connection callbacks
-        self.clock_sub = rospy.Subscriber(self.clock_topic, Clock, self.clock_cb)
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.clock_timer_cb)
-        self.timer.start(self.timer_period)
+        # Set up latency callback
+        self.latency_topic = rospy.get_param("latency_topic", "/mk3_core_connection/ping_data")
+        self.latency_sub = rospy.Subscriber(self.latency_topic, Ping, self.latency_cb)
 
+    def comms_cb(self, msg):
+        self.signal_strength.setText(str(msg.signal) + " dB")
 
-    def clock_timer_cb(self):
-        if self.first_value:
-            self.last_received_time.data = self.read_time.data
-            self.first_value = False            
-        elif not self.first_value and self.get_first_msg:
-            time_diff = (self.read_time.data - self.last_received_time.data).to_sec()
- 
-            if time_diff == 0:
-                self.no_time_change += 1
-                if self.no_time_change > 1:
-                    if self.latency < self.lost_connection_threshold + 2:
-                        self.latency += 1  
-            else:
-                self.no_time_change = 0
-                if self.latency > 0:
-                    self.latency -= 1
-            
-            if self.latency < self.poor_connection_threshold:
-                self.connection_status.setText("Good")
-            elif self.latency >= self.lost_connection_threshold:
-                self.connection_status.setText("Lost")
-            elif self.latency >= self.poor_connection_threshold and self.latency < self.lost_connection_threshold:
-                self.connection_status.setText("Poor")
-            
-            self.last_received_time.data = self.read_time.data   
-        
-    def clock_cb(self, msg):
-        self.clock_freq = msg.update_rate
-        if self.get_first_msg == False:
-            self.first_value = True
-            self.get_first_msg = True
-        self.read_time.data = msg.data
+    def latency_cb(self, msg):
+        self.latency.setText(format(msg.latency, ".2f") + " ms")
 
     def battery_cb(self, msg):
-        bat_level = format(msg.voltage, ".1f") + 'V'
+        bat_level = format(msg.voltage, ".1f") + ' V'
 
         if bat_level < self.low_voltage and not self.battery_state:
             self.battery_state = True
